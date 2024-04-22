1fad1f4ce62b5b011147b938ee4d82eb4e44a1d0
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-22 11:40:39 -0300

    Corrigir página inicial e adicionar título ao componente Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,7 @@
-import Footer from "../../components/footer/Footer";
-import Navbar from "../../components/navbar/Navbar";
-
 const Home = () => {
   return (
     <div className="flex flex-col justify-items-center">
-      <Navbar />
-      <p>Pagina incial</p>
-      <Footer />
+      <h1>Seja Bem Vindo ao FarmaAPI</h1>
     </div>
   );
 };
