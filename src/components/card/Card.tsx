2b36f82ad1d8e87333b672f4a84d46eba263fbf3
Gabriel Rodrigues <gabers357@gmail.com>
2024-04-22 10:16:00 -0300

    Adicionar componente Card ao projeto

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,54 @@
+import { ReactNode } from "react";
+
+interface CardProps {
+  title: string;
+  content?: string | ReactNode;
+  inFocus?: boolean;
+  secondaryButtonText?: string;
+  primaryButtonText?: string;
+  onClickPrimaryButton?: () => void;
+  onClickSecondaryButton?: () => void;
+}
+
+const Card = ({
+  title = "Title",
+  onClickPrimaryButton,
+  onClickSecondaryButton,
+  primaryButtonText = "Edit",
+  secondaryButtonText = "Delete",
+  inFocus,
+  content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate eu leo ac laoreet. Donec egestas tellus id vehicula fringilla. Phasellus varius et ante sed luctus.",
+}: CardProps) => {
+  return (
+    <div
+      className={`card card-compact w-96 bg-base-100 shadow-xl flex flex-col justify-between h-full ${
+        inFocus ? "z-50" : "z-0"
+      }`}
+    >
+      <div className="card-body flex items-center">
+        {title && <h2 className="card-title">{title}</h2>}
+        {content && <p>{content}</p>}
+      </div>
+      <div className="card-actions justify-center">
+        {onClickPrimaryButton && (
+          <button
+            onClick={onClickPrimaryButton}
+            className="btn btn-secondary flex-grow"
+          >
+            {secondaryButtonText}
+          </button>
+        )}
+        {onClickSecondaryButton && (
+          <button
+            onClick={onClickSecondaryButton}
+            className="btn btn-primary flex-grow"
+          >
+            {primaryButtonText}
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
